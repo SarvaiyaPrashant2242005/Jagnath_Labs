1131f48fcd9bf4d7331133276fe470feec22305f
SarvaiyaPrashant2242005 <prashanthere90@gmail.com>
2026-07-22 22:06:42 +0530

    debug

--- jagnath-ui/src/shared/services/apiEndpoints.js
+++ jagnath-ui/src/shared/services/apiEndpoints.js
@@ -7,6 +7,9 @@
  */
 
 // ─── Base URL ───────────────────────────────────────────────────────────────────
+console.log("ENV OBJECT:", import.meta.env);
+console.log("API URL:", import.meta.env.VITE_API_BASE_URL);
+
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 console.log(API_BASE_URL);
 // ─── Auth Endpoints ─────────────────────────────────────────────────────────────
