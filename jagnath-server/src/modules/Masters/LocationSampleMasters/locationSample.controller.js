d7defcd28e6e28a2e50a2634eab166a14af1a44a
SarvaiyaPrashant2242005 <prashanthere90@gmail.com>
2026-08-05 23:06:01 +0530

    Runnable

--- jagnath-server/src/modules/Masters/LocationSampleMasters/locationSample.controller.js
+++ jagnath-server/src/modules/Masters/LocationSampleMasters/locationSample.controller.js
@@ -76,8 +76,6 @@ const create = async (req, res) => {
         const locData = {
             name: value.name,
             status: value.status || "Active",
-            inlet: value.inlet !== undefined ? value.inlet : false,
-            outlet: value.outlet !== undefined ? value.outlet : false,
             companyId
         };
 
